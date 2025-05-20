7f742d8a18590981899ddb4a9032c56a01211fef
Rajeswari badiger <149804235+rajeswaribadiger2004@users.noreply.github.com>
2025-05-20 07:19:47 +0300

    Create Compound_interest.py

---
+++ Compound_interest.py
@@ -0,0 +1,21 @@
+
+def compound_interest(principal, rate, time):
+
+    # Calculates compound interest
+    Amount = principal * (pow((1 + rate / 100), time))
+    CI = Amount - principal
+    print("Compound interest is", CI)
+
+
+# Driver Code
+#Taking input from user.
+principal = int(input("Enter the principal amount: "))
+rate = int(input("Enter rate of interest: "))
+time = int(input("Enter time in years: " ))
+#Function Call
+compound_interest(principal,rate,time)
+
+"""
+A = P(1 + R/100)^ t 
+Compound Interest = A - P
+"""
